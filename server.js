43fb6a5a9fde125393f5f98a8efdfffc8ae1a815
kravilla <kravilla@live.unc.edu>
2021-12-04 18:00:16 -0500

    CREATE first edit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Set server port
 var HTTP_PORT = 5000
 // Start server
-app.listen(HTTP_PORT, () => {
+const server = app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
@@ -23,7 +23,11 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/new", (req,res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message":info.changes + " record created: ID" + info.lastInsertRowid});
+});
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
@@ -40,4 +44,12 @@ app.get("/app/users", (req, res) => {
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
     res.status(404);
+});
+
+// Tell STDOUT that the server is stopped
+process.on('SIGTERM', () => {
+	server.close(() => {
+		console.log('Server stopped');
+	});
+	
 });
